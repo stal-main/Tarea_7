4a9cb5dcad0fa51273685af0e00f6afdca4d9ef3
Kevin Solano <7stalyakov7@gmail.com>
2026-06-03 01:24:43 -0600

    Update BSTDictionary.h

--- BSTDictionary.h
+++ BSTDictionary.h
@@ -57,8 +57,162 @@ public:
 		}
 	}
 
-	List<Pair<K, V>>* getElements() {
-		return tree->getElements();
+	void setValue(K key, V value) override {
+		
+		if (!tree->contains(Pair<K, V>(key))) {
+
+			throw runtime_error("Key not found");
+		}
+
+		tree->remove(Pair<K, V>(key));
+
+		tree->insert(Pair<K, V>(key, value));
+	}
+
+	bool contains(K key) override {
+
+		return tree->contains(Pair<K, V>(key));
+	}
+
+	bool isEmpty() override {
+
+		return tree->isEmpty();
+	}
+
+	void clear() override {
+
+		tree->clear();
+	}	
+
+	List<K>* getKeys() override {
+
+		List<Pair<K, V>>* pairs = tree->getElements();
+
+		List<K>* keys = new LinkedList<K>();
+
+		pairs->goToStart();
+
+		while (!pairs->atEnd()) {
+
+			keys->append(pairs->getElement().key);
+
+			pairs->next();
+		}
+
+		delete pairs;
+
+		return keys;
+	}
+
+	List<V>* getValues() override {
+
+		List<Pair<K, V>>* pairs = tree->getElements();
+
+		List<V>* values = new LinkedList<V>();
+
+		pairs->goToStart();
+
+		while (!pairs->atEnd()) {
+
+			values->append(pairs->getElement().value);
+
+			pairs->next();
+		}
+
+		delete pairs;
+
+		return values;
+	}
+
+	int getSize() override {
+
+		return tree->getSize();
+	}
+
+	void print() override {
+
+		List<Pair<K, V>>* pairs = tree->getElements();
+
+		pairs->goToStart();
+
+		cout << "{ ";
+
+		bool first = true;
+
+		while (!pairs->atEnd()) {
+
+			if (!first) {
+
+				cout << ", ";
+			}
+
+			Pair<K, V> pair = pairs->getElement();
+
+			cout << pair.key << ": " << pair.value;
+
+			first = false;
+
+			pairs->next();
+		}
+
+		cout << " }" << endl;
+
+		delete pairs;
+	}
+
+	void update(Dictionary<K, V>* D) override {
+
+		List<K>* keys = D->getKeys();
+
+		keys->goToStart();
+
+		while (!keys->atEnd()) {
+
+			K key = keys->getElement();
+
+			V value = D->getValue(key);
+
+			if (contains(key) {
+
+				setValue(key, value);
+			}
+			else {
+
+				insert(key, value);
+			}
+
+			keys->next();
+		}
+
+		delete keys;
+	}
+
+	void zip(List<K>* keys, List<V>* values) override {
+		
+		keys->goToStart();
+
+		values->goToStart();
+
+		while (!keys->atEnd() && !values->atEnd()) {
+
+			K key = keys->getElement();
+
+			V value = values->getElement();
+
+			if (contains(key)) {
+
+				setValue(key, value);
+			}
+
+			else {
+
+				insert(key, value);
+			}
+
+			keys->next();
+
+			values->next();
+		}
 	}
 };
 
